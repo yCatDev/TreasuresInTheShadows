88564cd5a4e558df323cc94dc21a815e16b7b6e3
Grebenuk Denis <dphoenix002@gmail.com>
2020-01-28 18:24:23 +0200

    Init

---
+++ Resources/Tiled/TitS_main.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="TitS_main" tilewidth="16" tileheight="16" tilecount="176" columns="16">
+ <image source="../../Assets/Sprites/Game_Assets.png" width="264" height="176"/>
+</tileset>
